db1deaf62a550063eb0060d55d8be2f18ec898e5
Richard Bidin <richardbidin@outlook.com>
2023-09-04 23:35:53 -0300

    Adds cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import { routes } from './routes/routes';
 
 const app = express();
 
+app.use(cors());
 app.use(routes);
 
 export { app };
